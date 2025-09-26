0ae619180c4f3e8ca1dcec81aff4ed55f75679a7
Alok <aloks101296@gmail.com>
2025-09-26 11:13:19 +0530

    new changes

--- src/language/basics/HelloWord.java
+++ src/language/basics/HelloWord.java
@@ -3,8 +3,8 @@ package language.basics;
 public class HelloWord {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("hello word");
+		System		System		System		System		System
 	}
 
 }
